e45d713bf745034e8328367b88f2832eba84688c
Daniil Naumov <mrli0n@yandex.ru>
2026-02-24 14:58:03 +0300

    Добавил класс Achievement, UserAchievement и UserAchievementProgress.

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserAccountManager(BaseUserManager):
@@ -31,3 +32,48 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    target = models.PositiveIntegerField()
+    icon = models.ImageField(upload_to='achievements/icons/')
+    reward = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class UserAchievement(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='achievements'
+    )
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    get_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'achievement')
+
+    def __str__(self):
+        return f"{self.user.email} - {self.achievement.name}"
+
+
+class UserAchievementProgress(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='progressToAchievement'
+    )
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    current_value = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'achievement')
+
+    def __str__(self):
+        return f"{self.user.email} progress for {self.achievement.name}"
+
+    @property
+    def is_completed(self):
+        return self.current_value >= self.achievement.target
