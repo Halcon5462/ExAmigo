6c4bcfc193a7ce285e9deaec6c57a64959ceebd5
Артём Ткаченко <dlya-artema5462@mail.ru>
2026-03-01 00:54:22 +0300

    решение конфликта слияния из-за Taskserializer

--- backend/taskBank/serializers.py
+++ backend/taskBank/serializers.py
@@ -1,11 +1,38 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, TaskCorrectAnswer
+from account.models import TaskProgress
+
+
+class TaskCorrectAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskCorrectAnswer
+        fields = ["id", "answer_text"]
+
 
 class TaskSerializer(serializers.ModelSerializer):
+    correct_answers = TaskCorrectAnswerSerializer(many=True)
+    already_solved = serializers.SerializerMethodField()
     author_name = serializers.CharField(source="author.name", read_only=True)
     author_email = serializers.EmailField(source="author.email", read_only=True)
 
     class Meta:
         model = Task
         fields = '__all__'
-        extra_kwargs = {"author": {"read_only": True}}
+
+    def create(self, validated_data):
+        answers_data = validated_data.pop("correct_answers")
+        task = Task.objects.create(**validated_data)
+
+        for answer in answers_data:
+            TaskCorrectAnswer.objects.create(
+                task=task,
+                **answer
+            )
+
+        return task
+    
+    def get_already_solved(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return TaskProgress.objects.filter(user=user, task=obj).exists()
+        return False
