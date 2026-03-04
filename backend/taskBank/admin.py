3688f2d6a8310c079203c53b9be1e60c9694daca
Артём Ткаченко <dlya-artema5462@mail.ru>
2026-03-04 18:43:07 +0300

    Обновление админки для taskBank

--- backend/taskBank/admin.py
+++ backend/taskBank/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Task, TaskCorrectAnswer
+from .models import (
+    Task,
+    TaskCorrectAnswer,
+    TaskSet,
+    TaskSetItem
+)
 
 
 class TaskCorrectAnswerInline(admin.TabularInline):
@@ -7,6 +12,23 @@ class TaskCorrectAnswerInline(admin.TabularInline):
     extra = 1
 
 
+class TaskSetItemInline(admin.TabularInline):
+    model = TaskSetItem
+    extra = 1
+    autocomplete_fields = ["task"]
+
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    list_display = ("subject", "order_KIM", "difficulty", "author")
+    list_filter = ("subject", "difficulty")
+    search_fields = ("description",)
     inlines = [TaskCorrectAnswerInline]
+
+
+@admin.register(TaskSet)
+class TaskSetAdmin(admin.ModelAdmin):
+    list_display = ("name", "subject", "is_public", "author")
+    list_filter = ("subject", "is_public")
+    search_fields = ("name",)
+    inlines = [TaskSetItemInline]
