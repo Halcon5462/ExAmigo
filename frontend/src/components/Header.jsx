7038c799463894e7e0a02bbb14a0b02ef4ba0d5d
Артём Ткаченко <dlya-artema5462@mail.ru>
2026-03-09 19:30:32 +0300

    Рефакторинг маршрутизации фронтенда, стилизация header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import '../static/css/header.css'
+import '../static/css/style.css'
+const Header = () => {
+    return(
+        <header>
+            <div class="logo">
+                <img src="/images/logo.svg" />
+                <h2>Exam Service</h2>
+            </div>
+            <nav>
+                <a href="/">Главная</a>
+                <a href="/tasks">Банк заданий</a>
+                <a href="/tasks/create">Создать задание</a>
+                <a href="/shop">Магазин</a>
+                <a href="/profile">Профиль</a>
+                <a href="/tasksets">Список комплектов</a>
+                <a href="/tasksets/create">Создать комплект</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
