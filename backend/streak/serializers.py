6ea6c3610e7a0815852f17947936c9ac4904f67f
Grigory Balikhin <grigorybalikhin@gmail.com>
2026-03-15 06:14:40 +0000

    Добавил файл serializers.py

---
+++ backend/streak/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import UserStreak
+
+class UserStreakSerializer(serializers.ModelSerializer):
+    user_email = serializers.ReadOnlyField(source='user.email')
+    user_name = serializers.ReadOnlyField(source='user.name')
+    
+    class Meta:
+        model = UserStreak
+        fields = [
+            'id',
+            'user',
+            'user_email',
+            'user_name',
+            'current_streak',
+            'best_streak',
+            'last_activity_date',
+            'updated_at'
+        ]
+        read_only_fields = ['user', 'current_streak', 'best_streak', 'last_activity_date']
