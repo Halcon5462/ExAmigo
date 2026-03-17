5154360e33b386ecf5d7db9847e41a7a9920153c
Chugunin Ilia <icugunin@gmail.com>
2026-03-18 00:24:58 +0300

    feat(taskBank)#30 обновил urls.py

--- backend/taskBank/urls.py
+++ backend/taskBank/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, TaskSetViewSet
+from .views import TaskViewSet, TaskSetViewSet, StartExamView, FinishExamView, ExamSessionDetailView
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 router.register(r'tasksets', TaskSetViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('tasksets/<int:pk>/start-exam/', StartExamView.as_view(), name='start-exam'),
+    path('exams/<int:exam_id>/', ExamSessionDetailView.as_view(), name='exam-detail'),
+    path('exams/<int:exam_id>/finish/', FinishExamView.as_view(), name='finish-exam'),
+]
+
+urlpatterns += router.urls
