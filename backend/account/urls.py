f1edf033e20c881dfe877331c08ffb22d64c1e3e
Chugunin Ilia <icugunin@gmail.com>
2026-03-21 00:04:45 +0300

    feat(achievements)#20 перенёс ссылки и модели из приложения account в приложение achievements

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
-from .views import RegisterView, ProfileView, CustomTokenObtainPairView, UserAchievementListView, UserProgressListView
 from rest_framework_simplejwt.views import TokenRefreshView
-from django.urls import path
+
 from account.views import TaskSubmitView
+from .views import CustomTokenObtainPairView, ProfileView, RegisterView
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('user-achievements/', UserAchievementListView.as_view(), name='user-achievements'),
-    path('user-progress/', UserProgressListView.as_view(), name='user-progress'),
     path('task-progress/<int:pk>/submit/', TaskSubmitView.as_view(), name='task-submit'),
-    
 ]
