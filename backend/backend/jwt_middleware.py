902074e833105f6b25e64557a2479f5d40ff5b65
kuzmin maxim <mt162738153@gmail.com>
2026-05-03 15:50:58 +0000

    refactor(backend) #60 рефакторинг jwt под pylint

--- backend/backend/jwt_middleware.py
+++ backend/backend/jwt_middleware.py
@@ -1,11 +1,15 @@
-from channels.middleware import BaseMiddleware
-from channels.db import database_sync_to_async
-from django.contrib.auth.models import AnonymousUser
-from rest_framework_simplejwt.tokens import UntypedToken
-from django.contrib.auth import get_user_model
 from urllib.parse import parse_qs
-from jwt import decode as jwt_decode
+
+from channels.db import database_sync_to_async
+from channels.middleware import BaseMiddleware
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import ObjectDoesNotExist
+from jwt import DecodeError
+from jwt import decode as jwt_decode
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.tokens import UntypedToken
 
 User = get_user_model()
 
@@ -14,19 +18,34 @@ class JWTAuthMiddleware(BaseMiddleware):
     """
     Поддержка JWT из query string ?token=<ACCESS_TOKEN>
     """
+
     async def __call__(self, scope, receive, send):
         query_string = scope.get("query_string", b"").decode()
         token_list = parse_qs(query_string).get("token")
+
         if token_list:
             token = token_list[0]
+
             try:
-                UntypedToken(token)  # проверка токена
-                decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+                UntypedToken(token)
+                decoded_data = jwt_decode(
+                    token,
+                    settings.SECRET_KEY,
+                    algorithms=["HS256"],
+                )
+
                 user_id = decoded_data.get("user_id")
                 user = await database_sync_to_async(User.objects.get)(id=user_id)
                 scope["user"] = user
-            except Exception:
+
+            except (
+                InvalidToken,
+                TokenError,
+                DecodeError,
+                ObjectDoesNotExist,
+            ):
                 scope["user"] = AnonymousUser()
+
         else:
             scope["user"] = AnonymousUser()
 
