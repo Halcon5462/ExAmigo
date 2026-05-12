919bde165e99dbfdfdcae390133cdadfeac9d5af
Chugunin Ilia <icugunin@gmail.com>
2026-05-13 00:00:23 +0300

    feat(docs) #61 "Обновлен conf.py с расширениями для автоматической генерации документации"

--- docs/conf.py
+++ docs/conf.py
@@ -15,12 +15,22 @@ release = '1.0.0'
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autosummary_generate = True
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'inherited-members': True,
+}
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
